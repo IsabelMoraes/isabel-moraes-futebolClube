86a851be3a18d0c8b9b3f5ee086dc0876cd93b3d
Isabel <im03moraes@gmail.com>
2024-09-09 19:54:31 +0100

    aaaaaaa

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -5,37 +5,37 @@ import errorMiddleware from './middlewares/errorMiddleware';
 import teamController from './database/controllers/teamController';
 
 class App {
-
   public app: express.Express;
 
   constructor() {
-
     this.app = express();
     this.config();
 
     // Não remover essa rota
 
-    this.app.get('/', (req: any, res: { json: (arg0: { ok: boolean; }) => any; }) => res.json({ ok: true }));
+    this.app.get('/', (req: unknown, res: { json: (arg0: { ok: boolean; }) =>
+    unknown; }) => res.json({ ok: true }));
     this.app.use('/teams', teamController);
-    
+
     // Não remova esse middleware de erro, mas fique a vontade para customizá-lo
     // Mantenha ele sempre como o último middleware a ser chamado
     this.app.use(errorMiddleware);
   }
 
   private config(): void {
-  
-    const accessControl: express.RequestHandler = (_req: any, res: { header: (arg0: string, arg1: string) => void; }, next: () => void) => {
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
-      res.header('Access-Control-Allow-Headers', '*');
-      next();
-    };
-  
+    const accessControl: express.
+      RequestHandler = (_req: unknown, res: { header: (arg0: string, arg1: string) =>
+      void; }, next: () => void) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
+        res.header('Access-Control-Allow-Headers', '*');
+        next();
+      };
+
     this.app.use(express.json());
     this.app.use(accessControl);
   }
-  
+
   public start(PORT: string | number): void {
     this.app.listen(PORT, () => console.log(`Running on port ${PORT}`));
   }
